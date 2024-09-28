7f2b79e4f4e5960aa359a545edce58059df7a2e6
bradyDarren <dcgbfinancial@gmail.com>
2024-09-28 14:29:45 -0500

    Declaration of the upper, lower, symbols, and nums variables from string module

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -4,10 +4,16 @@ import string
 
 alpha_upper = string.ascii_uppercase
 alpha_lower = string.ascii_lowercase
+symbols = string.punctuation
+nums = string.digits
 
-total_count = input("How many characters total would you like your password to be?")
+rand_alpha = random.choice()
 
-alpha_count = input("How many alphabetical characters would you like to be included in your password? : ")
-num_count = input("How many numerical characters would you like to be included in your password? : ")
-symbol_count = input("How man symbols would you like included in your password? : ")
+print(rand_alpha)
+
+# total_count = input("How many characters total would you like your password to be?")
+
+# alpha_count = input("How many alphabetical characters would you like to be included in your password? : ")
+# num_count = input("How many numerical characters would you like to be included in your password? : ")
+# symbol_count = input("How man symbols would you like included in your password? : ")
 
