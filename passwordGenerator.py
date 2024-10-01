6d092d527a669a9270097ba6f4434243ecbfc277
bradyDarren <dcgbfinancial@gmail.com>
2024-09-30 22:37:10 -0500

    Addition of number function for random number selection.

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -7,28 +7,34 @@ alpha_lower = string.ascii_lowercase
 symbols = string.punctuation
 nums = string.digits
 
-total_count = input("How many characters total would you like your password to be?")
+# total_count = input("How many characters total would you like your password to be?")
 
-alpha_count = input("How many alphabetical characters would you like to be included in your password? : ")
-num_count = input("How many numerical characters would you like to be included in your password? : ")
-symbol_count = input("How man symbols would you like included in your password? : ")
+# alpha_count = input("How many alphabetical characters would you like to be included in your password? : ")
+# num_count = input("How many numerical characters would you like to be included in your password? : ")
+# symbol_count = input("How man symbols would you like included in your password? : ")
 
-# Function that will make a choice between an uppercase alpha character or a lowercase alpha character.
-def upper_or_lower(upper_list, lower_list):
+# # Function that will make a choice between an uppercase alpha character or a lowercase alpha character.
+# def upper_or_lower(upper_list, lower_list):
 
-    alpha_choice = [upper_list, lower_list]
-    list_options = random.choice(alpha_choice)
-    if list_options == upper_list: 
-        return random.choice(upper_list)
-    elif list_options == lower_list:
-        return random.choice(lower_list)
+#     alpha_choice = [upper_list, lower_list]
+#     list_options = random.choice(alpha_choice)
+#     if list_options == upper_list: 
+#         return random.choice(upper_list)
+#     elif list_options == lower_list:
+#         return random.choice(lower_list)
 
-# Function that will choose a random symbol and remove backticks (``), backslashes(\), and forwardslashes (/)
-def symbol(symbol_list):
-    unapproved_symbols = ['`','/','\\']
-    while True: 
-        rand_symbol = random.choice(symbol_list)
-        if rand_symbol in unapproved_symbols:
-            continue
-        else: 
-            return rand_symbol
+# # Function that will choose a random symbol and remove backticks (``), backslashes(\), and forwardslashes (/)
+# def symbol(symbol_list):
+#     unapproved_symbols = ['`','/','\\']
+#     while True: 
+#         rand_symbol = random.choice(symbol_list)
+#         if rand_symbol in unapproved_symbols:
+#             continue
+#         else: 
+#             return rand_symbol
+
+def number(nums_list):
+    rand_nums = random.choice(nums)
+    return rand_nums
+
+print(number(nums))
